ac3c46b47e2fb1c43b7400a484d97c4de12979fc
Fidel  Nieto <nietofidel25@gmail.com>
2024-10-05 00:21:30 +0000

    doing the database model for starwars

--- src/models.py
+++ src/models.py
@@ -7,26 +7,79 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'characters'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String, nullable=False)
+    height = Column(String)
+    mass = Column(String)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
+    gender = Column(String)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    favorites = relationship("Favorite", back_populates="character")
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicles'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String, nullable=False)
+    model = Column(String)
+    manufacturer = Column(String)
+    cost_in_credits = Column(String)
+    length = Column(String)
+    max_atmosphering_speed = Column(String)
+    crew = Column(String)
+    passengers = Column(String)
+    cargo_capacity = Column(String)
+    consumables = Column(String)
+    vehicle_class = Column(String)
 
-    def to_dict(self):
-        return {}
+    favorites = relationship("Favorite", back_populates="vehicle")
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    rotation_period = Column(String)
+    orbital_period = Column(String)
+    diameter = Column(String)
+    climate = Column(String)
+    gravity = Column(String)
+    terrain = Column(String)
+    population = Column(String)
+
+    favorites = relationship("Favorite", back_populates="planet")
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+
+    favorites = relationship("Favorite", back_populates="user")
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Character")
+    vehicle = relationship("Vehicle")
+    planet = relationship("Planet")
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
